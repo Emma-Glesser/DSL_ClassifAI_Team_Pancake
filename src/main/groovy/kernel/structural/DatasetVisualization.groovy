631b6325b14314e1b822b8e709146226f4e2811c
Laurie-Fernandez <59823387+Laurie-Fernandez@users.noreply.github.com>
2023-01-16 11:48:18 +0100

    Visualization et sous-classes

---
+++ src/main/groovy/kernel/structural/DatasetVisualization.groovy
@@ -0,0 +1,13 @@
+package kernel.structural
+
+class DatasetVisualization extends Visualization {
+    private String set_to_visualize;
+
+    String getSet_to_visualize() {
+        return set_to_visualize
+    }
+
+    void setSet_to_visualize(String set_to_visualize) {
+        this.set_to_visualize = set_to_visualize
+    }
+}

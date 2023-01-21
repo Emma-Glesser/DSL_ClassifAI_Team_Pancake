85b420dc73a6688b357be27ef01009c019543e58
Emma-Glesser <emma.glesser@etu.univ-cotedazur.fr>
2023-01-21 19:58:27 +0100

    Update Selection.groovy

--- src/main/groovy/kernel/structural/dataProcessing/Selection.groovy
+++ src/main/groovy/kernel/structural/dataProcessing/Selection.groovy
@@ -28,7 +28,9 @@ class Selection extends ProcessingStep {
 
     String getCode() {
         return String.format("\"# Data selection\\n\",\n" +
-                "    \"X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size = %f, shuffle=%s)\\n\\n\"",
+                "    \"Y = dataset[\'label\']\\n\",\n" +
+                "    \"X = dataset.drop(columns=[\'label\'])\\n\",\n" +
+                "    \"X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = %f, shuffle=%s)\\n\\n\"",
                 this.getTestSize(), this.getShuffleData() ? "True" : "False")
     }
 }
